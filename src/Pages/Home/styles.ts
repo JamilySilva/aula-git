5d4b5340f9747b1376523c8fc7c07d48b8896d6c
JamilySilva <jamily.gameplays@gmail.com>
2021-10-01 16:17:51 -0400

    Commit tela home finalizada

--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,63 +1,67 @@
-import {StyleSheet} from 'react-native';
-export const stylesLink=StyleSheet.create({
-	text:{
-		marginTop:10,
-		color:'#4A4A4C',
-		fontSize:13.71
-	}
+import { StyleSheet } from "react-native";
+
+export const stylesLink = StyleSheet.create({
+  text: {
+    //backgroundColor:"red",
+    marginTop: 10,
+    color: "#4A4A4C",
+    fontSize: 13.71,
+  },
 });
-export const stylesLinkImportantes=StyleSheet.create({
-	container:{
-		paddingVertical:10,
-		justifyContent:"space-between",
-		paddingHorizontal:40,
-		borderBottomColor:"blue",
-		flexDirection:"row",
-		borderBottomWidth:1
-	}
+
+export const stylesLinkImportantes = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+
+    borderBottomColor: "#F0F0F0",
+    borderBottomWidth: 1,
+  },
 });
-export const styles=StyleSheet.create({
-	container:{
-		flex:1
-	},
-	header:{
-		minHeight:94,
-		justifyContent:"space-between",
-		paddingTop:35,
-		flexDirection:"row",
-		backgroundColor:"#4369B0",
-		paddingHorizontal:25
-	},
-	inputTextView:{
-		paddingLeft:12,
-		borderRadius:25,
-		alignItems:"center",
-		height:45,
-		flex:1,
-		marginHorizontal:12,
-		backgroundColor:"#2C4877",
-		flexDirection:"row"
-	},
-	textInput: {
-		flex:1,
-		color:"#F5FFFF",
-		paddingRight:10
-	},
-	botao: {
-		borderRadius: 25,
-		height: 40,
-		backgroundColor: "#4369B0",
-		alignItems: "center",
-		marginHorizontal: 40,
-		justifyContent: "center",
-	},
-	conteudo: {
-		paddingVertical: 40,
-		justifyContent: "flex-end",
-		flex: 1,
-	}, 
-	textBtn: {
-		fontSize: 18,
-		color: "#F5FFFF",
-	},
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    minHeight: 93,
+    backgroundColor: "#4369B0",
+    paddingTop: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+  },
+  inputTextView: {
+    flex: 1,
+    backgroundColor: "#2C4877",
+    height: 40,
+    marginHorizontal: 10,
+    alignItems: "center",
+    borderRadius: 28,
+    flexDirection: "row",
+  },
+  textInput: {
+    color: "#F5FFFF",
+    // maxWidth:200
+    flex: 1,
+    paddingRight: 10,
+  },
+  meubotao: {
+    backgroundColor: "#4369B0",
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 40,
+    borderRadius: 10,
+  },
+  conteudoFacebook: {
+    flex: 1,
+    // backgroundColor:"#F0F500",
+    justifyContent: "flex-end",
+  },
+  meubotaoTexto: {
+    color: "#F5FFFF",
+  },
 });
